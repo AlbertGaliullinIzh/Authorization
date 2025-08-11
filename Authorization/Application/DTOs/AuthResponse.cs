9f292424d1c98639fdb84d807e19cfac845b19f3
Albert-Work_PC <galiullin.a.izh18@gmail.com>
2025-08-11 11:50:28 +0400

    Приведение проекта к необходимой архитектуре

---
+++ Authorization/Application/DTOs/AuthResponse.cs
@@ -0,0 +1,14 @@
+﻿using Authorization.Domain.Entities;
+
+namespace Authorization.Application.DTOs
+{
+    public record AuthResponse
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public int ExpiresIn { get; set; }
+        public User User { get; set; }
+    }
+}
