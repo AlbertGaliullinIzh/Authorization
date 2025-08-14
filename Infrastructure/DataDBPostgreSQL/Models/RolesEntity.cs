4918ba0a08eb041bdb74bc3c657c33ccf9b4b3f6
Альберт Галиуллин <agaliullin12@gmail.com>
2025-08-14 16:08:00 +0400

    Исправлены почти все слои

--- Infrastructure/DataDBPostgreSQL/Models/RolesEntity.cs
+++ Infrastructure/DataDBPostgreSQL/Models/RolesEntity.cs
@@ -6,6 +6,7 @@ namespace Infrastructure.DataDBPostgreSQL.Models
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public UserEntity User { get; set; }
+        public ICollection<UserEntity> Users { get; set; } = new List<UserEntity>();
+
     }
 }
